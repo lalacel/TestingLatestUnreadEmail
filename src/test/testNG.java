62711305254364577fa0938b52bc9398e3c0cea8
holyvia <holyviamarcellaaa@gmail.com>
2024-02-09 21:24:16 +0700

    feat: add proxy-server, addAssertEquals

--- src/test/testNG.java
+++ src/test/testNG.java
@@ -2,7 +2,6 @@ package test;
 import org.testng.annotations.Test;
 
 import pages.AllMailPage;
-import pages.GoogleAccountPage;
 import pages.InputMailPage;
 import pages.InputPasswordPage;
 
@@ -11,39 +10,38 @@ import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
 import java.util.Collections;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class testNG {
 	WebDriver driver;
 
+
   @Test
-  public void f() {
+  public void mainTest() {
 	ChromeOptions options = new ChromeOptions();
+	Proxy proxy = new Proxy();
+    proxy.setHttpProxy("<103.167.68.75:6363>");
+    options.setCapability("proxy", proxy);
 	options.addArguments("--incognito");
     options.addArguments("--disable-blink-features=AutomationControlled");;
 	options.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));
 	options.setExperimentalOption("useAutomationExtension", false);
 	
-	this.driver = new ChromeDriver(options);
 	
+	this.driver = new ChromeDriver(options);
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 	// Cast WebDriver to JavascriptExecutor
     JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
 
     // Execute the JavaScript code
     jsExecutor.executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
-    
     
 	driver.get("https://mail.google.com/mail/");
 
@@ -53,7 +51,6 @@ public class testNG {
 	
 	AllMailPage allMailPage = new AllMailPage(driver);
 	
-	GoogleAccountPage googleAccountPage = new GoogleAccountPage(driver);
 	
 	String email = "gslcwebprog@gmail.com";
 	String password = "Int3gr1ty!";
@@ -62,24 +59,14 @@ public class testNG {
 	mailPage.fillEmail(email);
 	mailPage.clickNext();
 	
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-	
 	passwordPage.fillPassword(password);
 	passwordPage.clickNext();
-	
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	
 	allMailPage.fillSearchField("is:unread");
 	allMailPage.clickNext();
 	String stringResult = allMailPage.getLastUnreadTitle();
-	Boolean testResult = stringResult.equals(title);
-	if(testResult) {
-		System.out.println("Testing Success! (testing result is equals to expected result)");
-	}
-	else {
-		System.out.println("Testing Failed! (testing result is not equals to expected result)");
-	}
-	
+
+	Assert.assertEquals(stringResult, title); 
   }
   @BeforeMethod
   public void beforeMethod() {
